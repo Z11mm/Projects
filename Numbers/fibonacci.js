d0b20cc48fb997475ae172ff1ca9614a7f5a6dba
Ghad <zimobie@gmail.com>
2019-08-07 21:51:53 +0100

    Added skeletal html for fibonacci script

--- Numbers/fibonacci.js
+++ Numbers/fibonacci.js
@@ -2,21 +2,31 @@
 Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
 */
 
-const fiboSequence = (number) => {
-    // an array is created with the nos in the seq that do not change
-    // a fibonacci sequence is created and the required seq generated from it
+const generateFibonacci = () => {
+    const input = document.querySelector('.user-input').value;
+    const validatedNumber = input.match(/[0-9]+/g);
     let arr = [0, 1];
     let result = [];
-    for (let i = 2; i <= number; i++) {
-        arr.push(arr[i - 2] + arr[i - 1]);
-    }
 
-    for (let i = 0; i <= number; i++) {
-        if (arr[i] < number) {
+    const fiboSequence = () => {
+        // an array is created with the nos in the seq that do not change
+        // a fibonacci sequence is created and the required seq generated from it
+        for (let i = 2; i <= validatedNumber; i++) {
+            arr.push(arr[i - 2] + arr[i - 1]);
+        }
+        return arr;
+    }
+    fiboSequence();
+
+    for (let i = 0; i <= validatedNumber; i++) {
+        if (arr[i] < validatedNumber) {
             result.push(arr[i]);
         }
     }
-    return result;
+    return result.toString();
 }
 
-console.log(fiboSequence(24));
+document.querySelector('.btn').addEventListener('click', (e) => {
+
+    console.log(generateFibonacci());
+});
--- Numbers/prime.js
+++ Numbers/prime.js
@@ -2,3 +2,22 @@
 Have the user enter a number and find all Prime Factors (if there are any) and display them.
 */
 
+const primeFactors = (number) => {
+    let primes = [1, 2, 3];
+    let primeFactors = [];
+
+    for (let i = 4; i <= number; i++) {
+        if (i % 2 === 1) {
+            primes.push(i);
+        }
+    }
+
+    for (let i = 0; i < primes.length; i++) {
+        if (number % primes[i] === 0) {
+            primeFactors.push(primes[i]);
+        }
+    }
+    return primeFactors;
+}
+
+console.log(primeFactors(24));
