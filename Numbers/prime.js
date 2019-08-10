ac7ab5e359e6e41bf6026a4b558a33b8bb10c9aa
Ghad <zimobie@gmail.com>
2019-08-10 13:23:51 +0100

    Added a brief description comment

--- Numbers/prime.js
+++ Numbers/prime.js
@@ -1,6 +1,8 @@
 /* 
 Have the user enter a number and find all Prime Factors (if there are any) and display them.
 */
+
+// A function to check for primes within number and then generate prime factors
 const checkForPrime = (i) => {
     for (let j = 2; j < i; j++ ) {
         if (i % j === 0) {
